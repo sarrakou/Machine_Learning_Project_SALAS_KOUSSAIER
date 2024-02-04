d59475b61576e56cedaa943ddd524d86cf45d0fb
AdrianaS13 <61713273+AdrianaS13@users.noreply.github.com>
2024-02-04 23:30:25 +0100

    Final RBFNetwork.h

--- HandwrittenLetterRecognition/RBFNetwork.h
+++ HandwrittenLetterRecognition/RBFNetwork.h
@@ -18,26 +18,18 @@ public:
     }
 
     void initializeCentersKMeans(const std::vector<std::vector<float>>& trainingInputs, int numCenters);
-
-    void runKMeans(const std::vector<std::vector<float>>& trainingInputs, int numCenters);
-
-    int findClosestCenter(const std::vector<float>& input);
-
-    void train(const std::vector<std::vector<float>>& trainingInputs, const std::vector<std::vector<float>>& trainingTargets);
-
     void initializeWeights(const std::vector<std::vector<float>>& targets);
+    float calculateDistanceSquared(const std::vector<float>& v1, const std::vector<float>& v2);
+    //void runKMeans(const std::vector<std::vector<float>>& trainingInputs, int numCenters);
+    int findClosestCenter(const std::vector<float>& input);
+    void train(const std::vector<std::vector<float>>& trainingInputs, const std::vector<std::vector<float>>& trainingTargets);
     float calculateRBF(float distance);
-
     std::vector<float> processInput(const std::vector<float>& input, const std::vector<std::vector<float>>& targets);
-
     float test(const std::vector<std::vector<float>>& testInputs, const std::vector<std::vector<float>>& testTargets);
-
     bool isPredictionCorrect(const std::vector<float>& output, const std::vector<float>& target);
 private:
     std::vector<float> calculateError(const std::vector<float>& output, const std::vector<float>& target);
-
     void updateWeights(const std::vector<float>& input, const std::vector<float>& error, float learningRate);
-
     float calculateTotalError(const std::vector<float>& error);
     float calculateDistance(const std::vector<float>& v1, const std::vector<float>& v2);
 };
